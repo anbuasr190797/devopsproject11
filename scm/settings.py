d3e8d34e107f72a1180f237b0eaadb0fff6b0013
EC2 Default User <ec2-user@ip-172-31-16-57.eu-west-1.compute.internal>
2024-04-06 15:59:05 +0000

    33rd commit

--- scm/settings.py
+++ scm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'czha6a=ow(iw^4%^orzg$)lo1riu@a(=abn)g(^g1scf8$qt6='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-scm-env.eba-5ixqgkjr.eu-west-1.elasticbeanstalk.com",
+ALLOWED_HOSTS = ["scm-env.eba-5ixqgkjr.eu-west-1.elasticbeanstalk.com",
 "9d21efeacc5146ba85516d1b5e06f787.vfs.cloud9.eu-west-1.amazonaws.com"]
 
 
