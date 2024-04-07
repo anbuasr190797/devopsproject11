b5099f60e246696ce61d0a8b7aa032f7c9697fa6
EC2 Default User <ec2-user@ip-172-31-16-57.eu-west-1.compute.internal>
2024-04-07 22:30:14 +0000

    38th commit

--- scm/settings.py
+++ scm/settings.py
@@ -21,7 +21,7 @@ STATIC_DIR = os.path.join(BASE_DIR,"static")
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'czha6a=ow(iw^4%^orzg$)lo1riu@a(=abn)g(^g1scf8$qt6='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
